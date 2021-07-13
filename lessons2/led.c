973608fee3de1bccfb6ae014cba0859fe60b3ee3
lyj <73394753+lyjxdu@users.noreply.github.com>
2021-07-13 10:19:35 +0800

    Update led.c

--- lessons2/led.c
+++ lessons2/led.c
@@ -1,1 +1,31 @@
+// C++ code
+//
+int i;
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(0, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
 
+void loop()
+{
+  for(i=0;i<14;i++)
+  {
+    digitalWrite(i, HIGH);
+    delay(200); // Wait for 1000 millisecond(s)
+    digitalWrite(i, LOW);
+    delay(200);// for 1000 millisecond(s)
+  }
+}
