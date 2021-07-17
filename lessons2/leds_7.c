da63c19012b52e3476bf6ea66aa5b89973b31290
lyj <73394753+lyjxdu@users.noreply.github.com>
2021-07-17 09:23:15 +0800

    Update leds_7.c

--- lessons2/leds_7.c
+++ lessons2/leds_7.c
@@ -21,7 +21,7 @@ void loop()
   {
     case 0:
     	digitalWrite(2, HIGH);
-        digitalWrite(3, HIGH);
+      digitalWrite(3, HIGH);
     	digitalWrite(4, HIGH);
      	digitalWrite(5, HIGH);
      	digitalWrite(6, HIGH);
@@ -35,80 +35,80 @@ void loop()
     	digitalWrite(7, LOW);
     	break;
     case 1:
-        digitalWrite(3, HIGH);
+      digitalWrite(3, HIGH);
     	digitalWrite(4, HIGH);
     	delay(1000);
     	digitalWrite(3, LOW);
     	digitalWrite(4, LOW);
     	break;
     case 2:
-        digitalWrite(3, HIGH);
+      digitalWrite(3, HIGH);
     	digitalWrite(2, HIGH);
-        digitalWrite(5, HIGH);
+      digitalWrite(5, HIGH);
     	digitalWrite(6, HIGH);
-        digitalWrite(8, HIGH);
+      digitalWrite(8, HIGH);
     	delay(1000);
-        digitalWrite(3, LOW);
+      digitalWrite(3, LOW);
     	digitalWrite(2, LOW);
-        digitalWrite(5, LOW);
+      digitalWrite(5, LOW);
     	digitalWrite(6, LOW);
-        digitalWrite(8, LOW);
+      digitalWrite(8, LOW);
     	break;
     case 3:
-        digitalWrite(3, HIGH);
+      digitalWrite(3, HIGH);
     	digitalWrite(2, HIGH);
-        digitalWrite(5, HIGH);
+      digitalWrite(5, HIGH);
     	digitalWrite(4, HIGH);
-        digitalWrite(8, HIGH);
+      digitalWrite(8, HIGH);
     	delay(1000);
-        digitalWrite(3, LOW);
+      digitalWrite(3, LOW);
     	digitalWrite(2, LOW);
-        digitalWrite(5, LOW);
+      digitalWrite(5, LOW);
     	digitalWrite(4, LOW);
-        digitalWrite(8, LOW);
+      digitalWrite(8, LOW);
     	break;
     case 4:
-        digitalWrite(3, HIGH);
+      digitalWrite(3, HIGH);
     	digitalWrite(7, HIGH);
     	digitalWrite(4, HIGH);
-        digitalWrite(8, HIGH);
+      digitalWrite(8, HIGH);
     	delay(1000);
-        digitalWrite(3, LOW);
+      digitalWrite(3, LOW);
     	digitalWrite(7, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(8, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(8, LOW);
     	break;
     case 5:
-        digitalWrite(7, HIGH);
+      digitalWrite(7, HIGH);
     	digitalWrite(2, HIGH);
-        digitalWrite(5, HIGH);
+      digitalWrite(5, HIGH);
     	digitalWrite(4, HIGH);
-        digitalWrite(8, HIGH);
+      digitalWrite(8, HIGH);
     	delay(1000);
-        digitalWrite(7, LOW);
+      digitalWrite(7, LOW);
     	digitalWrite(2, LOW);
-        digitalWrite(4, LOW);
+      digitalWrite(4, LOW);
     	digitalWrite(5, LOW);
-        digitalWrite(8, LOW);
+      digitalWrite(8, LOW);
     	break;
     case 6:
-        digitalWrite(7, HIGH);
+      digitalWrite(7, HIGH);
     	digitalWrite(2, HIGH);
-        digitalWrite(5, HIGH);
+      digitalWrite(5, HIGH);
     	digitalWrite(4, HIGH);
-        digitalWrite(8, HIGH);
+      digitalWrite(8, HIGH);
     	digitalWrite(6, HIGH);
     	delay(1000);
-        digitalWrite(7, LOW);
+      digitalWrite(7, LOW);
     	digitalWrite(2, LOW);
-        digitalWrite(4, LOW);
+      digitalWrite(4, LOW);
     	digitalWrite(5, LOW);
-        digitalWrite(8, LOW);
+      digitalWrite(8, LOW);
     	digitalWrite(6, LOW);
     	break;
     case 7:
     	digitalWrite(2, HIGH);
-        digitalWrite(3, HIGH);
+      digitalWrite(3, HIGH);
     	digitalWrite(4, HIGH);
     	delay(1000);
     	digitalWrite(2, LOW);
@@ -116,22 +116,35 @@ void loop()
     	digitalWrite(4, LOW);
     	break;
     case 8:
-        digitalWrite(7, HIGH);
+      digitalWrite(7, HIGH);
     	digitalWrite(3, HIGH);
-    	digitalWrite(2, HIGH);
-        digitalWrite(5, HIGH);
+      digitalWrite(2, HIGH);
+      digitalWrite(5, HIGH);
     	digitalWrite(4, HIGH);
-        digitalWrite(8, HIGH);
+      digitalWrite(8, HIGH);
     	digitalWrite(6, HIGH);
     	delay(1000);
-        digitalWrite(7, LOW);
+      digitalWrite(7, LOW);
     	digitalWrite(3, LOW);
     	digitalWrite(2, LOW);
-        digitalWrite(4, LOW);
+      digitalWrite(4, LOW);
     	digitalWrite(5, LOW);
-        digitalWrite(8, LOW);
+      digitalWrite(8, LOW);
     	digitalWrite(6, LOW);
     	break;
-    	
+    	case 9:
+        digitalWrite(7, HIGH);
+        digitalWrite(2, HIGH);
+        digitalWrite(5, HIGH);
+    	  digitalWrite(4, HIGH);
+        digitalWrite(8, HIGH);
+    	  digitalWrite(3, HIGH);
+    	  delay(1000);
+        digitalWrite(7, LOW);
+    	  digitalWrite(2, LOW);
+        digitalWrite(4, LOW);
+    	  digitalWrite(5, LOW);
+        digitalWrite(8, LOW);
+    	  digitalWrite(3, LOW);
   }
 }
